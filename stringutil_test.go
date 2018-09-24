14f3e80aa19c1f4de057e69bc1ee7ba7d2cc3217
Vahid Jafari <vahid.62jafari@gmail.com>
2018-09-24 14:48:45 +0330

    This is my test

---
+++ stringutil_test.go
@@ -0,0 +1,13 @@
+package gocommon
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestToPersian convert string to standard persian unicode
+func TestToPersian(t *testing.T) {
+	assert.Equal(t, 123, 123, "they should be equal")
+
+}
